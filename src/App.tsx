9fd8a550b84bf37d48b7f09ea041268181d7d6a1
augustok99 <carlosaugustox6@gmail.com>
2025-12-25 15:40:56 -0400

    aula 27

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
+import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+
 import './styles/theme.css'
 import './styles/global.css'
-import { Container } from './components/Container'
-import { Heading } from './components/Heading'
+import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
+
 
 function App() {
   return <>
     <Container>
-      <Heading>LOGO</Heading>
+      <Logo />
     </Container>
 
     <Container>
-      <Heading>Menu</Heading>
+      <Menu />
+    </Container>
+
+    <Container>
+      <CountDown />
+    </Container>
+
+    <Container>
+      <form className="form" action="">
+        <div className="formRow">
+          <DefaultInput labelText='Qualquer Coisa' id='meuInput' type="text" />
+        </div>
+
+         <div className="formRow">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+      </form>
     </Container>
 
     </>
-  
 }
 
 export default App
