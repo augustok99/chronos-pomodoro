e395aed71550b56d4dd19edd6a46f271615b869a
augustok99 <carlosaugustox6@gmail.com>
2025-12-28 21:26:23 -0400

    aula-41

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,34 +1,79 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
-import styles from './styles.module.css'
-import { useState } from 'react'
+import {
+  HistoryIcon,
+  HouseIcon,
+  SettingsIcon,
+  SunIcon,
+  MoonIcon,
+} from "lucide-react";
+import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
 
-type AvailableThemes = 'dark' | 'light';
-
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-    const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
 
-    function handleThemeChange(event : React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-        event.preventDefault(); // Não segue o link padrão
-        console.log('Clicado', Date.now());
-    }
+    return storageTheme;
+  });
 
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
-    return (
-        <nav className={styles.menu}>
-            <a className={styles.menuLink} href="#" aria-label='Ir para a página inicial' title='Ir para a Home'>
-                <HouseIcon />
-            </a>
-            <a className={styles.menuLink} href="#" aria-label='Ver histórico' title='Ver Histórico'>
-                <HistoryIcon />
-            </a>
-            <a className={styles.menuLink} href="#" aria-label='Configurações' title='Configurações'>
-                <SettingsIcon />
-            </a>
-            <a className={styles.menuLink} href="#" aria-label="Mudar tema" title='Mudar tema' onClick={handleThemeChange}>
-                <SunIcon />
-            </a>
-        </nav>
-    );
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a página inicial"
+        title="Ir para a Home"
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver histórico"
+        title="Ver Histórico"
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar tema"
+        title="Mudar tema"
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
 }
-
